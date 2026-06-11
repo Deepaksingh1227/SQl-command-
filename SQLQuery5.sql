775570a2e43da03701f1e206cca2ef580a6a3651
deepaksingh1227 <divyanthakur856@gmail.com>
2026-06-11 18:04:54 +0530

    DML -INSERT command

---
+++ SQLQuery5.sql
@@ -0,0 +1,3 @@
+--Delete the table persons from the dataBase--
+
+DROP TABLE persons
---
+++ SQLQuery6.sql
@@ -0,0 +1,10 @@
+-- Use of DMl commands 
+
+INSERT INTO customers(id, first_name, country, score)
+VALUES
+	(6,'Deepak', 'INDIA', 1000),
+	(7,'Shiv', 'INDIA', 950)
+
+
+	SELECT *
+	FROM customers
