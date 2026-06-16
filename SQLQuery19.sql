35bce89311b909e5505e7111d5b389139aa1db62
deepaksingh1227 <divyanthakur856@gmail.com>
2026-06-16 18:37:30 +0530

    Cross join also cartersion join only for special case of join to find all possible combinations

---
+++ SQLQuery19.sql
@@ -0,0 +1,7 @@
+
+---------------CROSS join------------
+
+--combines every row from left with every row from right all possible combination ---cartersion join-------
+SELECT *
+FROM customers
+CROSS JOIN orders
